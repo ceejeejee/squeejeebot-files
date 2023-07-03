d3e7cd22bac112eb8394d09aa21e9a3df471bd23
Charles Williams <charles.williams.9607@gmail.com>
2023-07-03 14:56:59 -0400

    bot can copy text and image links

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const {Client, IntentsBitField} = require("discord.js");
+const {Client, IntentsBitField, Embed} = require("discord.js");
 
 const client = new Client({
     intents:    [
@@ -18,6 +18,15 @@ client.on("messageCreate", async (message) => {
     console.log(message)
 
     if(!message?.author.bot){
-        message.author.send(`Echo ${message.content}`);
+        message.delete()
+            .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+            .catch(console.error);
+        
+        message.channel.send(`${message.author.username} said ${message.content}`);
+        if(message?.attachments !== undefined){
+            const attachmentsMap = message.attachments;
+            attachmentsMap.forEach(elem => message.channel.send(elem.url));
+            
+        }
     }
 });
