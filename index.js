530794133f6f9f928d5e04dd17d22b27f505b22c
Charles Williams <cjwillia@mit.edu>
2023-07-07 15:33:08 -0400

    added libs and can send resixed pictures

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
-const {Client, IntentsBitField, Embed} = require("discord.js");
+const {Client, IntentsBitField, Embed, Attachment, AttachmentBuilder} = require("discord.js");
+const { Image } = require('image-js');
 
 const client = new Client({
     intents:    [
@@ -15,18 +16,35 @@ const client = new Client({
 client.login(process.env.DISCORD_TOKEN);
 
 client.on("messageCreate", async (message) => {
-    console.log(message)
+    console.log(message);
 
     if(!message?.author.bot){
-        message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author.username}`))
-            .catch(console.error);
         
-        message.channel.send(`${message.author.username} said ${message.content}`);
         if(message?.attachments !== undefined){
+            if(message.content !== ''){
+                message.channel.send({content: `${message.author.username} said ${message.content}`}).catch(error => console.log(error));
+            }
+
             const attachmentsMap = message.attachments;
-            attachmentsMap.forEach(elem => message.channel.send(elem.url));
-            
+            attachmentsMap.map(async elem => {
+                try{
+                    const data = await fetch(elem.url);
+                    const buffer = await data.arrayBuffer();
+                    let image = await Image.load(buffer);
+                    image.resize({ width: 200 });
+                    await image.save('temp.png');
+                    const attachment = new AttachmentBuilder('temp.png');
+                    message.channel.send({files: [attachment]})
+                    .then(
+                        message.delete()
+                            .then(msg => console.log(`Deleted message from ${msg.author.username}`)))
+                    .catch(error => console.log(error));
+                }catch(error){
+                    console.log(error);
+                    message.channel.send("couldn't process image! sowwy,,,");
+                }
+            });
         }
+        
     }
 });
