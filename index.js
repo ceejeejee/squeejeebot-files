fdc830c4ab58ead51ccce3b7795913fefab16b56
Charles Williams <charles.williams.9607@gmail.com>
2023-07-08 19:08:47 -0400

    creates temp folder and deletes file after sending

--- index.js
+++ index.js
@@ -1,76 +1,131 @@
-// require('dotenv').config()
+import dotenv from 'dotenv';
+dotenv.config();
 
-// const {Client, IntentsBitField, Embed, Attachment, AttachmentBuilder} = require("discord.js");
-// const { Image } = require('image-js');
-// const fs = require('fs');
-// const { execFile } = require('child_process');
-import dotenv from 'dotenv'
-dotenv.config()
-import {Client, IntentsBitField, Embed, Attachment, AttachmentBuilder} from "discord.js";
+import {Client, IntentsBitField, Embed, Attachment, AttachmentBuilder, Collection, Events, GatewayIntentBits} from "discord.js";
 import { Image } from'image-js';
 import fs from 'fs';
 import { execFile } from 'child_process';
 import gifsicle from "gifsicle";
-
-const SUPPORTED_IMGS = ['.png', '.jpg', '.TIFF'];
+import path from 'node:path';
 
 const client = new Client({
     intents:    [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.DirectMessages,
-        IntentsBitField.Flags.MessageContent,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.MessageContent,
     ],
 });
+
+// client.commands = new Collection();
+
+// const commandsPath = './commands';
+// const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+// // for (const file of commandFiles) {
+// // 	const filePath = path.join(commandsPath, file);
+// // 	const command = from filePath;
+// // 	// Set a new item in the Collection with the key as the command name and the value as the exported module
+// // 	if ('data' in command && 'execute' in command) {
+// // 		client.commands.set(command.data.name, command);
+// // 	} else {
+// // 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+// // 	}
+// // }
+// const cmdFiles = fs.readdir("./commands", (err, files) =>{
+//     files.forEach((f) => {
+//         if (!f.endsWith(".js")) return;
+//             const response = client.loadCommand(f);
+//         if (response) {
+//             console.log(response);
+//         }
+//     });
+// });
+
+
+const SUPPORTED_IMGS = ['.png', '.jpg', '.TIFF'];
 
 client.login(process.env.DISCORD_TOKEN);
 
 client.on("messageCreate", async (message) => {
     // console.log(message);
 
+    // resend text messages & file attachments
     if(!message?.author.bot){
         
         if(message?.attachments !== undefined){
+            // resend text message if message.content is not empty
             if(message.content !== ''){
                 message.channel.send({content: `${message.author.username} said ${message.content}`}).catch(error => console.log(error));
+                message.delete().then(msg => console.log(`Deleted message from ${msg.author.username}`));
             }
-
-            const attachmentsMap = message.attachments;
             
+            // resends attachments if they can be processed
             message.attachments.forEach(async a => {
                 try{
+                    const path = `./${message.guild}/${message.channel}/${message.author.username}`;
                     const data = await fetch(a.url);
                     const buffer = new DataView(await data.arrayBuffer());
                     const fileExt = a.name.slice(a.name.lastIndexOf('.'));
-                    fs.writeFileSync(`./temp${fileExt}`, buffer);
-                    const filename = await resize(fileExt);
-                    console.log(filename);
+                    const filename = `./temp/${message.guild.name}${message.channel.name}_${message.author.username}${fileExt}`;
+
+                    fs.writeFileSync(filename, buffer);
+                    await resize(filename, fileExt);
+                    console.log(`saved as: ${filename}`);
 
                     const attachment = new AttachmentBuilder(filename);
                     message.channel.send({files: [attachment]})
                         .then(
                             message.delete()
-                                .then(msg => console.log(`Deleted message from ${msg.author.username}`)))
-                        .catch(error => console.log(error));
+                                .then(msg => console.log(`Deleted message from ${msg.author.username}`)));
+                    fs.rm(filename, (err) => {
+                        if(err){
+                            // File deletion failed
+                            console.error(err.message);
+                            return;
+                        }
+                    });
 
                 }catch(error){
                     console.log(error);
-                    errorMessage();
+                    message.channel.send("couldn't process image :bangbang: sowwy,,,");
                 }
             });
         }     
     }
 });
 
-async function resize(fileExt){
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+async function resize(filename, fileExt){
     if(SUPPORTED_IMGS.includes(fileExt)){
         const resizePromise = new Promise(async (resolve) => {
-            let image = await Image.load(`./temp${fileExt}`);
+            let image = await Image.load(filename);
             image.resize({ width: 200 });
-            await image.save(`./temp${fileExt}`)
+            await image.save(filename)
             console.log('image resized!');
-            resolve(`./temp${fileExt}`);
+            resolve(filename);
         });
         return resizePromise;
             
@@ -79,7 +134,7 @@ async function resize(fileExt){
         const resizePromise = new Promise(async (resolve) => {
             console.time('execute time');
             // Gifsicle implementation: https://stackoverflow.com/questions/47138754/nodejs-animated-gif-resizing
-            execFile(gifsicle, ['--resize-fit-width', '300', '-o', `./temp_smol${fileExt}`, `./temp${fileExt}`], err => {
+            execFile(gifsicle, ['--resize-fit-width', '300', '-o', filename, filename], err => {
                 console.timeEnd('execute time');
 
                 if (err) {
@@ -87,20 +142,12 @@ async function resize(fileExt){
                 }
 
                 console.log('image resized!');
-                resolve(`./temp_smol${fileExt}`);
+                resolve(filename);
             });
         });
         return resizePromise;
 
     }else{
-        errorMessage();
+        throw new Error("couldn't process file")
     }
 }
-
-function errorMessage(){
-    message.channel.send("couldn't process image :bangbang: sowwy,,,");
-}
-
-// function replaceFile(filename){
-//     
-// }
